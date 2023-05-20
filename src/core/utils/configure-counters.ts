24f235f6052408504ec6ca5449472ac3db41ee55
pnik1ta <nikitap0zd3y3v@gmail.com>
2023-05-20 10:53:14 +0400

    Change footer and solved tasks counter

--- src/core/utils/configure-counters.ts
+++ src/core/utils/configure-counters.ts
@@ -2,7 +2,7 @@ import { Counter } from "../models/counter.model";
 
 export function configureCounters(): Counter[] {
   return [
-    new Counter({ start: 0, end: 36, interval: 50, increment: 1, }, 'Typescript solved tasks', 'https://github.com/PNik1ta/TS-tasks', 'Go to repository'),
+    new Counter({ start: 0, end: 38, interval: 50, increment: 1, }, 'Typescript solved tasks', 'https://github.com/PNik1ta/TS-tasks', 'Go to repository'),
     new Counter({ start: 0, end: 9, interval: 50, increment: 1, }, 'Courses finished'),
   ]
 };
