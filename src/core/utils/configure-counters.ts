8e95604eca2bea056a73ebf6579b8f9297816ee1
pnik1ta <nikitap0zd3y3v@gmail.com>
2023-05-30 10:17:01 +0400

    Change number of solved tasks

--- src/core/utils/configure-counters.ts
+++ src/core/utils/configure-counters.ts
@@ -2,7 +2,7 @@ import { Counter } from "../models/counter.model";
 
 export function configureCounters(): Counter[] {
   return [
-    new Counter({ start: 0, end: 43, interval: 50, increment: 1, }, 'Typescript solved tasks', 'https://github.com/PNik1ta/TS-tasks', 'Go to repository'),
+    new Counter({ start: 0, end: 46, interval: 50, increment: 1, }, 'Typescript solved tasks', 'https://github.com/PNik1ta/TS-tasks', 'Go to repository'),
     new Counter({ start: 0, end: 9, interval: 50, increment: 1, }, 'Courses finished'),
   ]
-};
+};  
